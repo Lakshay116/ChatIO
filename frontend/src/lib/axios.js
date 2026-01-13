2bc02bb807e13c783c23ced80e1d4f6a18fa686d
Lakshay116 <jangralakshay611@gmail.com>
2026-01-13 20:28:29 +0530

    axios path

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE = "development" ? "https://chatio-bp7g.onrender.com" : "/api",
+    baseURL: import.meta.env.MODE = "development" ? "https://chatio-bp7g.onrender.com/api" : "/api",
     withCredentials: true,
 })
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://chatio-bp7g.onrender.com" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://chatio-bp7g.onrender.com/" : "/";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
