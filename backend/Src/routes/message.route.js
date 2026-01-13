b42944fa8d2e501c192ed75c6facf077681d5bb9
Lakshay116 <jangralakshay611@gmail.com>
2026-01-13 09:54:16 +0530

    message route

--- backend/Src/routes/message.route.js
+++ backend/Src/routes/message.route.js
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { getAllContacts, getMessageByUserId, sendMessage, getChatPartners } from "../controllers/mesage.controller.js";
+import { getAllContacts, getMessagesByUserId, sendMessage, getChatPartners } from "../controllers/mesage.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js"
 import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
@@ -9,7 +9,7 @@ router.use(arcjetProtection, protectRoute);
 
 router.get("/contacts", getAllContacts);
 router.get("/chats", getChatPartners);
-router.get("/:id", getMessageByUserId);
+router.get("/:id", getMessagesByUserId);
 router.post("send/:id", sendMessage);
 
 
