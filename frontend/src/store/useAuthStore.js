99e00c8cdd768c34df92b1f38292522a68b49638
Lakshay116 <jangralakshay611@gmail.com>
2026-04-10 19:43:44 +0530

    mk

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,8 +3,8 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-// const BASE_URL = import.meta.env.MODE === "development" ? "https://chatio-bp7g.onrender.com/" : "http://localhost:3000/";
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://chatio-bp7g.onrender.com/" : "/";
+// const BASE_URL = "http://localhost:3000/";
 
 
 export const useAuthStore = create((set, get) => ({
